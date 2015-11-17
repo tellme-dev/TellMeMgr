236a9962571fb8747913f0c742097975ab24c1ec
xml777 <386002446@qq.com>
2015-11-17 11:16:06 +0800

    APP需要的数据接口包

---
+++ src/main/java/com/hotel/app/AdController.java
@@ -0,0 +1,10 @@
+package com.hotel.app;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/app/ad")
+public class AdController {
+
+}
---
+++ src/main/java/com/hotel/app/BbsController.java
@@ -0,0 +1,5 @@
+package com.hotel.app;
+
+public class BbsController {
+
+}
