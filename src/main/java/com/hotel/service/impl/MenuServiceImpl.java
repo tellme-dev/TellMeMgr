6077ed065f931391dde05a8760dcf04827ee06b1
SeeLittleGirlAgain <1309230548@qq.com>
2015-11-24 20:15:29 +0800

    1、短信验证

--- src/main/java/com/hotel/service/impl/MenuServiceImpl.java
+++ src/main/java/com/hotel/service/impl/MenuServiceImpl.java
@@ -3,11 +3,12 @@ package com.hotel.service.impl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.hotel.dao.MenuMapper;
 import com.hotel.model.Menu;
 import com.hotel.service.MenuService;
-
+@Service
 public class MenuServiceImpl implements MenuService {
 
 	@Override
---
+++ src/main/java/com/hotel/viewmodel/SmsInfo.java
@@ -0,0 +1,28 @@
+package com.hotel.viewmodel;
+
+import java.util.Date;
+
+public class SmsInfo {
+	private String mobile;
+	private Date sendTime;
+	private String verifCode;
+	public String getMobile() {
+		return mobile;
+	}
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+	public Date getSendTime() {
+		return sendTime;
+	}
+	public void setSendTime(Date sendTime) {
+		this.sendTime = sendTime;
+	}
+	public String getVerifCode() {
+		return verifCode;
+	}
+	public void setVerifCode(String verifCode) {
+		this.verifCode = verifCode;
+	}
+	
+}
