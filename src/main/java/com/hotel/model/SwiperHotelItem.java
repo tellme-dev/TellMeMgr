c8030fceca4a87776a1969f0d9f8e89e402dd585
Liutaixiong <531457277@qq.com>
2015-12-29 20:05:34 +0800

    首页

---
+++ src/main/java/com/hotel/model/SwiperHotelItem.java
@@ -0,0 +1,19 @@
+package com.hotel.model;
+
+public class SwiperHotelItem {
+	private int itemTagId;
+	private String imageUrl;
+	public int getItemTagId() {
+		return itemTagId;
+	}
+	public void setItemTagId(int itemTagId) {
+		this.itemTagId = itemTagId;
+	}
+	public String getImageUrl() {
+		return imageUrl;
+	}
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+	
+}
